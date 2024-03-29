13a495942a24eb51a2e34fb2b1373701639d508f
KOSIT AROMSAVA <kosit.arom@gmail.com>
2024-03-29 09:28:56 +0700

    commit vue-curd

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+         
+          My App
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs >
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
